db1fc83f5b1606ce0568db434e93eec3e29cfac5
stalg <stalg@LAPTOP-PSVUB72F>
2021-02-01 00:42:01 +0200

    class point

---
+++ snake1/point1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake1
+{
+    class point1
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
+
